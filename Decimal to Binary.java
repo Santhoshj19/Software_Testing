cf59a2392d33f791380a30468a174234976f9346
Santhoshj19 <122586541+Santhoshj19@users.noreply.github.com>
2024-03-05 12:45:47 +0530

    Create Decimal to Binary.java

---
+++ Decimal to Binary.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assert True;
+
+import java.util.Scanner,
+
+class binary
+
+public static void main(String[] args) {Scanner in new Scanner(System.in);
+
+int decimal in.nextInt();
+
+System.out.println("BINARY IS" + binary);
+
+System.out.print("OCTAL IS ");
+
+System.out.println(Integer.toOctalString(decimal));
+
+assertTrue(14 decimal);
+
+ }
+}
