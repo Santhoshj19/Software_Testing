ddcc4fdfe69e4b5498ed5a1de5029de65ae33b5e
Santhoshj19 <122586541+Santhoshj19@users.noreply.github.com>
2024-03-05 12:49:00 +0530

    Create Factorial number.java

---
+++ Factorial number.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.Scanner,
+
+class factorial
+
+{
+
+public static void main(String[] args)
+
+{ int i,j.pr-1;
+
+try{
+
+Scanner s new Scanner(System.in);
+
+System.out.println("Enter the number to find the factorial");
+
+int ns.nextInt():
+
+if(n<0)
+
+System.out.println("Invalid");
+
+}
+
+else if(n0)
+
+System.out.println("1");
+
+}
+
+else
+
+for(i=n;i>0;i--)
+
+{ propr*i;
+
+}
+
+System.out.println("The answer is:"+pr);
+
+assertTrue(120 pr);
+
+}
+
+catch(Exception e)
+
+1
+
+System.out.println("Invalid");
+
+}
+}
+}
